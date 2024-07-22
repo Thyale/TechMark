e6abf8fe03b87449f63c2a6a99c1e36a2455b999
Thyale MD <thyaledantas@gmail.com>
2024-07-22 10:14:11 -0300

    About e header finalizados

--- TechMarkProjetc/src/app/app.module.ts
+++ TechMarkProjetc/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { EmpresasComponent } from './components/empresas/empresas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ServicesComponent,
+    EmpresasComponent
   ],
   imports: [
     BrowserModule,
---
+++ TechMarkProjetc/src/app/components/empresas/empresas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empresas',
+  templateUrl: './empresas.component.html',
+  styleUrl: './empresas.component.css'
+})
+export class EmpresasComponent {
+
+}
