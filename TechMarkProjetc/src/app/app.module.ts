bc6b08c8cd8fcf3a949db0e0ac5521c7ecf5aca3
Thyale MD <thyaledantas@gmail.com>
2024-07-23 10:20:27 -0300

    Notificação de alerta

--- TechMarkProjetc/src/app/app.module.ts
+++ TechMarkProjetc/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +30,9 @@ import { ContatoComponent } from './components/contato/contato.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [
     provideClientHydration()
--- TechMarkProjetc/src/app/components/contato/contato.component.ts
+++ TechMarkProjetc/src/app/components/contato/contato.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contato',
@@ -6,21 +7,48 @@ import { Component } from '@angular/core';
   styleUrl: './contato.component.css'
 })
 export class ContatoComponent {
-  Nome: string = "";
-  Email: string = "";
-  texto: string = "";
+
+  InputNome: string = "";
+  InputEmail: string = "";
+  Inputtexto: string = "";
 
   duvida = {
-    nome: this.Nome,
-    email: this.Email,
-    texto: this.texto
+    nome: this.InputNome,
+    email: this.InputEmail,
+    texto: this.Inputtexto
   };
 
+  constructor(private toastr: ToastrService) {}
+
   EnviarContato() {
-    this.duvida.nome = this.Nome;
-    this.duvida.email = this.Email;
-    this.duvida.texto = this.texto;
-  
-    localStorage.setItem('BuscarDuvida', JSON.stringify(this.duvida));
+    if (this.InputNome === "" || this.InputEmail === "" || this.Inputtexto === "") {
+      this.toastr.error('Preencha todo o formulário antes de enviar', 'ERRO!', {
+        positionClass: 'toast-top-right',
+        timeOut: 4000,
+        progressBar: true,
+        progressAnimation: 'increasing',
+        tapToDismiss: true,
+        newestOnTop: true,
+        enableHtml: true,
+        toastClass: 'ngx-toastr custom-toast-error'
+      });
+    } else {
+      this.duvida.nome = this.InputNome;
+      this.duvida.email = this.InputEmail;
+      this.duvida.texto = this.Inputtexto;
+
+      localStorage.setItem('BuscarDuvida', JSON.stringify(this.duvida));
+
+      this.toastr.success('Em breve entraremos em contato', 'Mensagem recebida!', {
+        positionClass: 'toast-top-right',
+        timeOut: 4000,
+        progressBar: true,
+        progressAnimation: 'increasing',
+        tapToDismiss: true,
+        newestOnTop: true,
+        enableHtml: true,
+        toastClass: 'ngx-toastr custom-toast-success'
+      });
+    }
   }
 }
